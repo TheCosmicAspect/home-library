eaae009fef58b158e1f135cfcf8ff0ddc86f56c3
TheCosmicAspect <liam@theharmsfamily.org>
2025-07-22 00:43:31 -0500

    Added Tag Functionality

---
+++ src/forms/__init__.py
@@ -0,0 +1,1 @@
+from forms.fields import OptionalSelectField
--- src/forms/forms.py
+++ src/forms/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import FieldList, StringField, TextAreaField, SelectMultipleField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
+from . import OptionalSelectField
 
 class BookForm(FlaskForm):
     title = StringField('Title')
@@ -34,7 +35,7 @@ class AuthorForm(FlaskForm):
 class TagForm(FlaskForm):
     label = StringField('Label', validators=[DataRequired()])
     description = TextAreaField('Description')
-    parent = SelectField('Parent', coerce=int, validators=[DataRequired()])
+    parent = OptionalSelectField('Parent', coerce=int, validators=[Optional()])
     type = StringField('Type', validators=[Length(max=50)])
     submit = SubmitField('Submit')
 
